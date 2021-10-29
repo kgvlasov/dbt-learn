b4721845fff3034b286176d3289aa5117d665318
kgvlasov@gmail.com <kgvlasov@gmail.com>
2021-10-29 15:08:24 +0000

    123123

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -20,6 +20,12 @@ customer_orders as (
 
 ),
 
+lifetimevalue as (
+    select customer_id,sum(amount) as lifetime_value
+    from {{ref('fct_orders')}}
+    group by customer_id
+),
+
 
 final as (
 
@@ -29,11 +35,14 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        lifetimevalue.lifetime_value / 100 as lifetime_value
+
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join lifetimevalue using (customer_id)
 
 )
 
